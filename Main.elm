f93f3af62094bae7d54c8a6804ce97b459a5eaf7
JosephStevens <josephscottstevens@gmail.com>
2017-12-18 12:02:26 -0500

    more some code cleanup

--- Main.elm
+++ Main.elm
@@ -1,10 +1,9 @@
-module Main exposing (..)
+module Main exposing (main)
 
 import Dict
-import Html exposing (Html, div, p, text)
+import Html exposing (Html, div, text)
 import Html.Attributes exposing (style)
 import Keyboard
-import Piece exposing (Orientation(..), Piece(..), Shape(..))
 import Random
 import Random.Extra
 import RasterShapes as Raster exposing (Position, Size)
@@ -12,10 +11,12 @@ import Set exposing (Set)
 import Time
 
 
+numCols : Int
 numCols =
     10
 
 
+numRows : Int
 numRows =
     20
 
@@ -53,6 +54,12 @@ type Model
     = Uninitialized
     | Initialized State
     | GameOver Int
+    | Error String
+
+
+init : ( Model, Cmd Msg )
+init =
+    ( Uninitialized, Random.generate (uncurry Initialize) <| Random.pair pieceGenerator pieceGenerator )
 
 
 type Msg
@@ -67,16 +74,138 @@ type Msg
     | NoOp
 
 
-init : ( Model, Cmd Msg )
-init =
-    ( Uninitialized, Random.generate (uncurry Initialize) <| Random.pair pieceGenerator pieceGenerator )
+update : Msg -> Model -> ( Model, Cmd Msg )
+update msg model =
+    case model of
+        GameOver _ ->
+            ( model, Cmd.none )
+
+        Uninitialized ->
+            case msg of
+                Initialize currentPiece nextPiece ->
+                    let
+                        model =
+                            Initialized
+                                { currentScore = 0
+                                , currentPiece = currentPiece
+                                , currentPiecePosition = ( 0, 0 )
+                                , nextPiece = nextPiece
+                                , fixatedBlocks = Set.empty
+                                , dropping = False
+                                }
+                    in
+                    ( model, Cmd.none )
+
+                _ ->
+                    Error ("Somehow you managed to get a " ++ toString msg ++ " msg in an uninitialized state o_O") ! []
+
+        Initialized state ->
+            case msg of
+                Initialize _ _ ->
+                    Error "Somehow you managed to get an initialize msg in an initialized state o_O" ! []
+
+                NoOp ->
+                    ( model, Cmd.none )
+
+                MoveLeft ->
+                    ( Initialized <| moveCurrentPieceLeft state, Cmd.none )
+
+                MoveRight ->
+                    ( Initialized <| moveCurrentPieceRight state, Cmd.none )
+
+                Rotate ->
+                    ( Initialized <| rotateCurrentPiece state, Cmd.none )
+
+                Tick ->
+                    let
+                        newState =
+                            movePieceDown state
+                    in
+                    if detectCollisions newState then
+                        ( fixateAndAdvance state, Random.generate NextPiece pieceGenerator )
+                    else
+                        ( Initialized newState, Cmd.none )
+
+                Drop ->
+                    ( Initialized { state | dropping = True }, Cmd.none )
+
+                StopDrop ->
+                    ( Initialized { state | dropping = False }, Cmd.none )
+
+                NextPiece piece ->
+                    ( Initialized { state | nextPiece = piece }, Cmd.none )
+
+        Error err ->
+            model ! []
+
+
+view : Model -> Html Msg
+view model =
+    case model of
+        GameOver score ->
+            text <| "Game over! Your score was " ++ toString score
+
+        Uninitialized ->
+            text ""
+
+        Initialized state ->
+            div []
+                [ renderOutline
+                    |> pxSize 1
+                , renderBoard state.currentPiece state.currentPiecePosition (Set.toList state.fixatedBlocks)
+                    |> pxSize 20
+                , renderNext state.nextPiece
+                    |> pxSize 20
+                , pixelWithItems 1 (Position 240 110) [ text (toString state.currentScore) ]
+                ]
+
+        Error error ->
+            div [] [ text error ]
+
+
+main : Program Never Model Msg
+main =
+    Html.program
+        { init = init
+        , update = update
+        , subscriptions = subscriptions
+        , view = view
+        }
+
+
+type Orientation
+    = North
+    | South
+    | East
+    | West
+
+
+type Shape
+    = IShape
+    | JShape
+    | LShape
+    | OShape
+    | SShape
+    | TShape
+    | ZShape
+
+
+type Piece
+    = Piece Shape Orientation
+
+
+pxSize : Int -> List Position -> Html Msg
+pxSize size items =
+    items
+        |> List.map (pixel size)
+        |> div []
 
 
 anyFixated : State -> Int -> Bool
 anyFixated state offset =
     let
         blocks =
-            Piece.getBlocks state.currentPiece
+            getBlocks state.currentPiece
 
         ( cx, cy ) =
             state.currentPiecePosition
@@ -97,7 +226,7 @@ moveCurrentPieceLeft model =
             model.currentPiecePosition
 
         left =
-            Piece.getLeftOffset model.currentPiece
+            getLeftOffset model.currentPiece
     in
     if x + left <= 0 then
         model
@@ -114,7 +243,7 @@ moveCurrentPieceRight model =
             model.currentPiecePosition
 
         right =
-            Piece.getRightOffset model.currentPiece
+            getRightOffset model.currentPiece
     in
     if x + 4 - right >= numCols then
         model
@@ -131,10 +260,10 @@ rotateCurrentPiece model =
             model.currentPiecePosition
 
         newPiece =
-            Piece.rotate model.currentPiece
+            rotate model.currentPiece
 
         right =
-            Piece.getRightOffset newPiece
+            getRightOffset newPiece
     in
     if x + right > numCols then
         { model | currentPiece = newPiece, currentPiecePosition = ( numCols - right, y ) }
@@ -160,7 +289,7 @@ detectCollisions : State -> Bool
 detectCollisions state =
     let
         pieceBlocks =
-            List.map (translateRelativeTo state.currentPiecePosition) <| Piece.getBlocks state.currentPiece
+            List.map (translateRelativeTo state.currentPiecePosition) <| getBlocks state.currentPiece
     in
     List.any (\( _, y ) -> y >= numRows) pieceBlocks || List.any (\point -> Set.member point state.fixatedBlocks) pieceBlocks
 
@@ -169,7 +298,7 @@ fixate : State -> State
 fixate state =
     let
         pieceBlocks =
-            List.map (translateRelativeTo state.currentPiecePosition) <| Piece.getBlocks state.currentPiece
+            List.map (translateRelativeTo state.currentPiecePosition) <| getBlocks state.currentPiece
     in
     { state | fixatedBlocks = Set.union state.fixatedBlocks <| Set.fromList pieceBlocks }
 
@@ -224,10 +353,6 @@ checkForCompleteRows state =
             { state | fixatedBlocks = Set.fromList <| shiftedRows, currentScore = state.currentScore + 100 * Set.size completeRows }
 
 
-
--- XXX hardcoded value
-
-
 advance : State -> State
 advance state =
     { state | currentPiece = state.nextPiece, currentPiecePosition = ( 0, 0 ) }
@@ -248,70 +373,6 @@ checkGameOver state =
 fixateAndAdvance : State -> Model
 fixateAndAdvance state =
     checkGameOver <| advance <| checkForCompleteRows <| fixate <| state
-
-
-update : Msg -> Model -> ( Model, Cmd Msg )
-update msg model =
-    case model of
-        GameOver _ ->
-            ( model, Cmd.none )
-
-        Uninitialized ->
-            case msg of
-                Initialize currentPiece nextPiece ->
-                    let
-                        model =
-                            Initialized
-                                { currentScore = 0
-                                , currentPiece = currentPiece
-                                , currentPiecePosition = ( 0, 0 )
-                                , nextPiece = nextPiece
-                                , fixatedBlocks = Set.empty
-                                , dropping = False
-                                }
-                    in
-                    ( model, Cmd.none )
-
-                -- LTA
-                _ ->
-                    Debug.crash <| "Somehow you managed to get a " ++ toString msg ++ " msg in an uninitialized state o_O"
-
-        Initialized state ->
-            case msg of
-                -- LTA
-                Initialize _ _ ->
-                    Debug.crash "Somehow you managed to get an initialize msg in an initialized state o_O"
-
-                NoOp ->
-                    ( model, Cmd.none )
-
-                MoveLeft ->
-                    ( Initialized <| moveCurrentPieceLeft state, Cmd.none )
-
-                MoveRight ->
-                    ( Initialized <| moveCurrentPieceRight state, Cmd.none )
-
-                Rotate ->
-                    ( Initialized <| rotateCurrentPiece state, Cmd.none )
-
-                Tick ->
-                    let
-                        newState =
-                            movePieceDown state
-                    in
-                    if detectCollisions newState then
-                        ( fixateAndAdvance state, Random.generate NextPiece pieceGenerator )
-                    else
-                        ( Initialized newState, Cmd.none )
-
-                Drop ->
-                    ( Initialized { state | dropping = True }, Cmd.none )
-
-                StopDrop ->
-                    ( Initialized { state | dropping = False }, Cmd.none )
-
-                NextPiece piece ->
-                    ( Initialized { state | nextPiece = piece }, Cmd.none )
 
 
 translateKeyDown : Keyboard.KeyCode -> Msg
@@ -364,14 +425,22 @@ subscriptions model =
                 , Time.every tickInterval <| always Tick
                 ]
 
+        Error _ ->
+            Sub.none
+
 
 px : Int -> String
 px i =
     toString i ++ "px"
 
 
-pixel : Int -> Position -> Html Msg
-pixel size { x, y } =
+pixel : Int -> Position -> Html msg
+pixel size position =
+    pixelWithItems size position []
+
+
+pixelWithItems : Int -> Position -> List (Html msg) -> Html msg
+pixelWithItems size { x, y } t =
     div
         [ style
             [ ( "background", "#000000" )
@@ -382,7 +451,7 @@ pixel size { x, y } =
             , ( "position", "absolute" )
             ]
         ]
-        []
+        t
 
 
 renderOutline : List Position
@@ -401,7 +470,7 @@ renderBoard : Piece -> ( Int, Int ) -> List ( Int, Int ) -> List Position
 renderBoard currentPiece ( curX, curY ) fixatedBlocks =
     let
         currentBlock =
-            Piece.getBlocks currentPiece
+            getBlocks currentPiece
                 |> List.map (\( x, y ) -> Position (x + curX + 1) (y + curY + 1))
 
         blocks =
@@ -413,53 +482,322 @@ renderBoard currentPiece ( curX, curY ) fixatedBlocks =
 
 renderNext : Piece -> List Position
 renderNext nextPiece =
-    Piece.getBlocks nextPiece
+    getBlocks nextPiece
         |> List.map (\( x, y ) -> Position (x + 12) (y + 1))
 
 
-
--- renderScore : Int -> List Position
--- renderScore score =
---     Raster.circle
---         5
---         (Position 20 5)
--- XXX hardcoded value
-
-
-pxSize : Int -> List Position -> Html Msg
-pxSize size items =
-    items
-        |> List.map (pixel size)
-        |> div []
+verticalIShape : List (List number)
+verticalIShape =
+    [ [ 0, 1, 0, 0 ]
+    , [ 0, 1, 0, 0 ]
+    , [ 0, 1, 0, 0 ]
+    , [ 0, 1, 0, 0 ]
+    ]
 
 
-view : Model -> Html Msg
-view model =
-    case model of
-        GameOver score ->
-            text <| "Game over! Your score was " ++ toString score
-
-        Uninitialized ->
-            text ""
-
-        Initialized state ->
-            [ renderOutline
-                |> pxSize 1
-            , renderBoard state.currentPiece state.currentPiecePosition (Set.toList state.fixatedBlocks)
-                |> pxSize 20
-            , renderNext state.nextPiece
-                |> pxSize 20
-
-            --, renderScore state.currentScore <| 2
-            ]
-                |> div []
+horizontalIShape : List (List number)
+horizontalIShape =
+    [ [ 1, 1, 1, 1 ]
+    , [ 0, 0, 0, 0 ]
+    , [ 0, 0, 0, 0 ]
+    , [ 0, 0, 0, 0 ]
+    ]
 
 
-main : Program Never Model Msg
-main =
-    Html.program
-        { init = init
-        , update = update
-        , subscriptions = subscriptions
-        , view = view
-        }
+northLShape : List (List number)
+northLShape =
+    [ [ 0, 0, 0, 0 ]
+    , [ 1, 0, 0, 0 ]
+    , [ 1, 0, 0, 0 ]
+    , [ 1, 1, 0, 0 ]
+    ]
+
+
+eastLShape : List (List number)
+eastLShape =
+    [ [ 0, 0, 0, 0 ]
+    , [ 0, 0, 1, 0 ]
+    , [ 1, 1, 1, 0 ]
+    , [ 0, 0, 0, 0 ]
+    ]
+
+
+southLShape : List (List number)
+southLShape =
+    [ [ 0, 0, 0, 0 ]
+    , [ 1, 1, 0, 0 ]
+    , [ 0, 1, 0, 0 ]
+    , [ 0, 1, 0, 0 ]
+    ]
+
+
+westLShape : List (List number)
+westLShape =
+    [ [ 0, 0, 0, 0 ]
+    , [ 1, 1, 1, 0 ]
+    , [ 1, 0, 0, 0 ]
+    , [ 0, 0, 0, 0 ]
+    ]
+
+
+northJShape : List (List number)
+northJShape =
+    [ [ 0, 0, 0, 0 ]
+    , [ 0, 1, 0, 0 ]
+    , [ 0, 1, 0, 0 ]
+    , [ 1, 1, 0, 0 ]
+    ]
+
+
+eastJShape : List (List number)
+eastJShape =
+    [ [ 0, 0, 0, 0 ]
+    , [ 1, 0, 0, 0 ]
+    , [ 1, 1, 1, 0 ]
+    , [ 0, 0, 0, 0 ]
+    ]
+
+
+southJShape : List (List number)
+southJShape =
+    [ [ 0, 0, 0, 0 ]
+    , [ 1, 1, 0, 0 ]
+    , [ 1, 0, 0, 0 ]
+    , [ 1, 0, 0, 0 ]
+    ]
+
+
+westJShape : List (List number)
+westJShape =
+    [ [ 0, 0, 0, 0 ]
+    , [ 1, 1, 1, 0 ]
+    , [ 0, 0, 1, 0 ]
+    , [ 0, 0, 0, 0 ]
+    ]
+
+
+oShape : List (List number)
+oShape =
+    [ [ 0, 0, 0, 0 ]
+    , [ 0, 1, 1, 0 ]
+    , [ 0, 1, 1, 0 ]
+    , [ 0, 0, 0, 0 ]
+    ]
+
+
+verticalSShape : List (List number)
+verticalSShape =
+    [ [ 0, 0, 0, 0 ]
+    , [ 0, 1, 1, 0 ]
+    , [ 1, 1, 0, 0 ]
+    , [ 0, 0, 0, 0 ]
+    ]
+
+
+horizontalSShape : List (List number)
+horizontalSShape =
+    [ [ 0, 0, 0, 0 ]
+    , [ 1, 0, 0, 0 ]
+    , [ 1, 1, 0, 0 ]
+    , [ 0, 1, 0, 0 ]
+    ]
+
+
+northTShape : List (List number)
+northTShape =
+    [ [ 0, 0, 0, 0 ]
+    , [ 1, 1, 1, 0 ]
+    , [ 0, 1, 0, 0 ]
+    , [ 0, 0, 0, 0 ]
+    ]
+
+
+eastTShape : List (List number)
+eastTShape =
+    [ [ 0, 1, 0, 0 ]
+    , [ 0, 1, 1, 0 ]
+    , [ 0, 1, 0, 0 ]
+    , [ 0, 0, 0, 0 ]
+    ]
+
+
+southTShape : List (List number)
+southTShape =
+    [ [ 0, 1, 0, 0 ]
+    , [ 1, 1, 1, 0 ]
+    , [ 0, 0, 0, 0 ]
+    , [ 0, 0, 0, 0 ]
+    ]
+
+
+westTShape : List (List number)
+westTShape =
+    [ [ 0, 1, 0, 0 ]
+    , [ 1, 1, 0, 0 ]
+    , [ 0, 1, 0, 0 ]
+    , [ 0, 0, 0, 0 ]
+    ]
+
+
+verticalZShape : List (List number)
+verticalZShape =
+    [ [ 0, 0, 0, 0 ]
+    , [ 1, 1, 0, 0 ]
+    , [ 0, 1, 1, 0 ]
+    , [ 0, 0, 0, 0 ]
+    ]
+
+
+horizontalZShape : List (List number)
+horizontalZShape =
+    [ [ 0, 0, 0, 0 ]
+    , [ 0, 1, 0, 0 ]
+    , [ 1, 1, 0, 0 ]
+    , [ 1, 0, 0, 0 ]
+    ]
+
+
+isVertical : Orientation -> Bool
+isVertical orient =
+    case orient of
+        North ->
+            True
+
+        South ->
+            True
+
+        East ->
+            False
+
+        West ->
+            False
+
+
+getLeftOffset : Piece -> Int
+getLeftOffset piece =
+    getShape piece
+        |> List.map (\t -> List.indexedMap (,) t)
+        |> List.map (\t -> List.filter (\( _, b ) -> b == 1) t)
+        |> List.map (\t -> List.map Tuple.first t)
+        |> List.map List.head
+        |> List.filterMap identity
+        |> List.minimum
+        |> Maybe.withDefault 0
+
+
+getRightOffset : Piece -> Int
+getRightOffset piece =
+    getShape piece
+        |> List.map (\t -> List.reverse t)
+        |> List.map (\t -> List.indexedMap (,) t)
+        |> List.map (\t -> List.filter (\( _, b ) -> b == 1) t)
+        |> List.map (\t -> List.map Tuple.first t)
+        |> List.map List.head
+        |> List.filterMap identity
+        |> List.minimum
+        |> Maybe.withDefault 0
+
+
+getShape : Piece -> List (List Int)
+getShape (Piece shape orientation) =
+    case shape of
+        IShape ->
+            if isVertical orientation then
+                verticalIShape
+            else
+                horizontalIShape
+
+        JShape ->
+            case orientation of
+                North ->
+                    northJShape
+
+                South ->
+                    southJShape
+
+                East ->
+                    eastJShape
+
+                West ->
+                    westJShape
+
+        LShape ->
+            case orientation of
+                North ->
+                    northLShape
+
+                South ->
+                    southLShape
+
+                East ->
+                    eastLShape
+
+                West ->
+                    westLShape
+
+        OShape ->
+            oShape
+
+        SShape ->
+            if isVertical orientation then
+                verticalSShape
+            else
+                horizontalSShape
+
+        TShape ->
+            case orientation of
+                North ->
+                    northTShape
+
+                South ->
+                    southTShape
+
+                East ->
+                    eastTShape
+
+                West ->
+                    westTShape
+
+        ZShape ->
+            if isVertical orientation then
+                verticalZShape
+            else
+                horizontalZShape
+
+
+rotate : Piece -> Piece
+rotate (Piece shape orient) =
+    case orient of
+        North ->
+            Piece shape East
+
+        East ->
+            Piece shape South
+
+        South ->
+            Piece shape West
+
+        West ->
+            Piece shape North
+
+
+getBlocks : Piece -> List ( Int, Int )
+getBlocks piece =
+    let
+        shape =
+            getShape piece
+    in
+    List.concat <|
+        List.concat <|
+            List.indexedMap
+                (\y row ->
+                    List.indexedMap
+                        (\x present ->
+                            if present == 1 then
+                                [ ( x, y ) ]
+                            else
+                                []
+                        )
+                        row
+                )
+                shape
